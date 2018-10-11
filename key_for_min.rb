02d47240828d2fbdfc07bd975af5598af68a0038
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-11 22:33:35 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+patriots = {"Tom Brady" => 12, "Julian Edelman" => 11, "Gronk" => 87}
+patriots.sort  do|a,b|
 end
