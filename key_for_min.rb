1127718e1229ad859baa7950378c0ba5fa507f68
Dr. XyClo <sdcarmody@gmail.com>
2019-01-08 17:52:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,25 @@ require 'pry'
 name_hash = {adam: 1, bill: 2, carly: 3}
 
 def key_for_min_value(name_hash)
+  
+  def key_for_min_value(name_hash)
+  # define two variables for comparison, one for current lowest key and another for current lowest value.
+  name_hash.each do |key, value|
+    if #value is less than the current lowest value
+      #assign value to lowest value variable
+      #assign key to lowest key variable
+    end
+  end
+  #return the lowest key variable
+end
 name_hash.inject do |key_value1a, key_value2a|  ## not sure when booleans are pass nil yet I can create a hash from the array 
 
    key_value1b = {key_value1a[0] => key_value1a[1]}
    key_value2b = {key_value2a[0] => key_value2a[1]}
    
-   # binding.pry  
+   puts key_value1b
+   puts key_value2b
+    # binding.pry  
     
  # key1a.each do |name1, value1a|
  #   key2a.each do |name2, value2a|
