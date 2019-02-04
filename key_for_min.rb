f230c3721b4495a4e9f354a304f2dac7b4c711f7
Bry Cox <carl.cc@gmail.com>
2019-02-04 17:04:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  arr = []
+  arr_b = []
+  name_hash.each {|x,y| arr << y}
+  if arr.length != 0
+    arr[0] < arr[1] ? arr_b << arr[0] : arr_b << arr[1]
+    arr[1] < arr[2] ? arr_b << arr[1] : arr_b << arr[2]
+    arr_b[0] < arr_b[1] ? name_hash.key(arr_b[0]) : name_hash.key(arr_b[1])
+  else
+   nil
+ end
 end
