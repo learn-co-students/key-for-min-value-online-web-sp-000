d7d7f6aa54a11f8ce15d183587a294556d3ae599
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-21 21:44:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ Nickname={:Daisy=> 18,:childhood=> 89, Island => 264}
+key_for_min_value(name_hash)
 end
