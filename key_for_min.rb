ca18efdd679d0c9c0e434d0f0bde3f919bfdf3ea
Ashley Miller <asm2fe@virginia.edu>
2019-04-03 00:07:19 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  key = ""
+  vmin = Integer.new.ceil
+  name_hash.each do |name, v|
+    if v < vmin
+      vmin = v
+      key = name
+    end
+  end
+  return key
 
 end
