ca2a2ea14461138b9a9b749e0e18b7ef60c6df3a
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 15:34:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+  the_key, val = name_hash.first
+  
+  name_hash.each do |key, value|
+    if value < val
+      val = value
+      the_key = key
+    end
+  end
+  
+  the_key
+  
 end
