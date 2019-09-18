d5b58423231b4aea117bcb7e23c4d3e72d5e0619
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-18 18:28:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowvalue = 0
+  lowkey = nil
+  
+  name_hash.each do |k, v|
+    if lowvalue == 0 || v < lowvalue
+      lowvalue = v
+      lowkey = k
 end
+end
+lowkey
+end
+    
