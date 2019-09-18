066f4314493c93d52877fcf5ab7a6ba693c0723c
Adam Weissman <adam@comedytragedyepic.com>
2019-09-18 11:30:44 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    return nil 
+  else
+  new = name_hash.to_a
+  compare_to_me = new[0]
+  counter = 0
+  until counter == new.length
+    new.each do |i|
+      if i[1] < compare_to_me[1]
+        compare_to_me = i
+        counter += 1
+      else
+        counter += 1
+      end
+    end
+  end
+  return compare_to_me[0]
+end
 end
