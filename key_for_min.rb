1e4c1077212f8b068e04968acfe4223d6e8b8aa8
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-07 18:39:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,5 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  low_num_name = nil
-  low_num = 0 
   
-  name_hash.each do |name, num|
-    if low_num == 0 || num < low_num
-      low_num = num 
-      low_num_name = name 
-    end 
-  end 
-  low_num_name
 end
