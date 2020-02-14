513b238067f23f6ba67a962355fbb380c585e95d
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-14 01:41:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+# name_hash.invert.max
+ i = name_hash.length
+  while name_hash.length > 1 do
+    name_hash.delete(name_hash.invert.max&.last)
+    i -= 1
+  end
+  name_hash.invert.max&.last
 
 end
