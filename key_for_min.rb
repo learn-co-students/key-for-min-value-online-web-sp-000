bf5165cf5e3f2cc4186ccbb41abc17107bd7e078
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-14 01:50:58 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,9 @@
 def key_for_min_value(name_hash)
 # name_hash.invert.max
  i = name_hash.length
-  while name_hash.length > 1 do
+  while i > 1 do
     name_hash.delete(name_hash.invert.max&.last)
     i -= 1
   end
   name_hash.invert.max&.last
-
 end
