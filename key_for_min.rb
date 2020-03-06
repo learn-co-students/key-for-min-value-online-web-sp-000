5c7e5df18d7e5aaa6420bb2e7a1fe866580a682b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 14:35:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(named_hash)
+named_array = named_hash.to_a
+  default_value= 1000
+  lowest_key_value= nil 
 
-end
+  named_array.each do |v|
+      if v[1] < default_value
+        default_value = v[1]
+        lowest_key_value = v[0]
+      end
+   end
+  return lowest_key_value
+end  
