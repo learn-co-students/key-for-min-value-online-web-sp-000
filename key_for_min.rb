45b06cb0aaeb2638819dfe3344a9f35dd79eb9cc
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-23 06:20:40 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,13 @@ def key_for_min_value(name_hash)
     nil
   else
     lowest_value = Float::INFINITY
+    lowest_key = ""
     name_hash.each do |key, value|
       if value < lowest_value
         lowest_value = value
+        lowest_key = key
       end
     end
-    lowest_value
+    lowest_key
   end
 end
