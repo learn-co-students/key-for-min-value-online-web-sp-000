209100f516047c0b026ac961133cf9a4d1b93009
Evan Burton <evan@heyevan.com>
2020-04-19 09:41:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,13 @@
+
 def key_for_min_value(name_hash)
-  def key_for_min_value(name_hash)
 
-    current_minimum_val = nil
-    current_key_assoc_w_min_val = nil
-
-    name_hash.each do |key, value|
-
-      if current_minimum_val == nil
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key
-      else
-      
-        if current_minimum_val > value
-          current_minimum_val = value
-          current_key_assoc_w_min_val = key
-        end
-      end
-    end
-    current_key_assoc_w_min_val
-  end 	end
+min_key = nil
+min_value = nil
+name_hash.each do |k, v|
+  if min_value == nil || min_value > v 
+    min_value = v
+    min_key = k
+  end
+end
+min_key
+end
