bdde4c88cf2cf2648723e129140cc499daa2b65b
Heath Mast <hrmast@gmail.com>
2020-05-04 03:40:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
+  minimum_val = nil
+  key_min_val = nil
+
+  name_hash.each do |key, value|
+
+    if minimum_val == nil
+      minimum_val = value
+      key_min_val = key
+    else
+
+      if minimum_val > value
+        minimum_val = value
+        key_min_val = key
+      end
+    end
+  end
+  key_min_val
 end
