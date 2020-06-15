ca70db282e882a34fdb8d23faf9743e27ae8d855
Seth Near <seth.j.near@gmail.com>
2020-06-15 16:28:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.length == 0
+    return nil
+  else
+  values = name_hash.collect {|key, value| value}
+  smallest = values[0]
+     values.each { |value|
+       if value < smallest
+         smallest = value
+       end}
+  name_hash.each {|key, value|
+    if value == smallest
+      return key
+    end
+  }
+end
 end
