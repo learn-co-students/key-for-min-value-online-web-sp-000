11400974e915607b8b638746638c6aaa153c848a
Lesly Juarez <leslyj2@gmail.com>
2020-06-22 14:04:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = nil
+  name_hash.each do |key, value| 
+    if min_value == nil 
+      min_value = key 
+    elsif value.to_i < name_hash[min_value].to_i
+      min_value = key 
+    end 
+  end
+  min_value
+  
 end
