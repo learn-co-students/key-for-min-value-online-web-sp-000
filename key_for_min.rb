69735de9ba004bb03a86a89deb8ea34a5571b475
fut0k0 <cccabell@icloud.com>
2020-10-01 14:17:18 -0700

    Passing lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+	min = nil
+	pair = {}
 
+	name_hash.each do |name, num|
+		min = min ||= num
+
+		if num <= min
+			min = num
+			pair = {name => num}
+		end
+	end
+
+	name_hash.map do |name, num|
+		num == min ? name : nil
+	end .compact[0]
 end
