9c490f94de8114222d685f57ed0afe37c165dcca
Nkenge Simms <simmsn@yahoo.com>
2020-10-01 22:42:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+ smallest_key=nil
+  smallest_value=nil
+  name_hash.each{|key,val|
+    if smallest_value==nil || val<smallest_value
+      smallest_value=val
+      smallest_key=key
+    end
+  }
+  smallest_key
 end
+
+  
+
+
