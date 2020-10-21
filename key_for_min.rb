cf846c6130cb1b85562bf390d79781fe2101a29b
Damian Malysza <damian.malysza@gmail.com>
2020-10-21 23:47:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   smallest_value = nil
   name_hash.each do |k,v|
-    if smallest_value == nil || smallest_value > v
+    if smallest_value == nil || v < smallest_value
       smallest_value = v
       smallest_key = k
     end
