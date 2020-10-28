6c5f7f91e74d1188f9028b7910aab73d697d5c26
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-28 05:14:51 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,13 +9,13 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if min_val === -1
       min_val = value
-      hash = key.to_s
+      hash = key
     elsif min_val > value
       min_val = value
-      hash = key.to_s
+      hash = key
     else
       next
     end
   end
-  hash.to_sym
+  hash
 end
