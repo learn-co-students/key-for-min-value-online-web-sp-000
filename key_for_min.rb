feea14ad262f8f5505eb8832761eb054a53bbf82
Kristina Frederick <kristinafrederick@icloud.com>
2020-12-13 02:42:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.inject { |name, number| name[1] > number[1] ? number : name }[0] unless name_hash.empty?
 end
