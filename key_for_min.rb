5e8e9f3a5e677fd14455f3974f2872e6e387750f
remote-torrent-6747 <abbyc@sky.com>
2020-12-23 12:35:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_value = nil
+  matching_key = nil
+  name_hash.each do |key, value|
+    if smallest_value == nil || value < smallest_value
+      smallest_value = value
+      matching_key = key
+    end
+  end
+  return matching_key
 end
