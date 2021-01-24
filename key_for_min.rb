5f36ec093b972dc50c63ea6457ab915f76560c96
Elliott Vargo <vargoelliott@gmail.com>
2021-01-24 00:13:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,26 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+def my_min_with_key(name_hash)
 
-def key_for_min_value(name_hash)
+
 
 end
+def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
+  else
+low = Float::INFINITY
+low_k = nil
+    name_hash.each do |key, value|
+      if value < low
+        low = value
+        low_k = key
+      end
+  end
+  low_k
+end
+end
+
+#this required a lot of research before discovering the Float::INFINITY comparison
+#other than this solution I couldn't find one I even tried nesting enumerables
+#but I couldnt get that to work
