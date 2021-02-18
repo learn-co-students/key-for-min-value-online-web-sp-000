af02a8605c66bb496bc72137c0f7f53a81909449
Jouse then <jthen93@gmail.com>
2021-02-18 00:55:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    return nil
+  else
+    lowest_value = nil
+    lowest_key = nil
+    
+    name_hash.each do |k, v|
+      if lowest_value == nil
+        lowest_value = v
+        lowest_key = k
+      elsif v < lowest_value
+        lowest_value = v
+        lowest_key = k
+      end
+    end
+    return lowest_key
+  end
 end
